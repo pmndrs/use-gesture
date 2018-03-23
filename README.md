a95665e969c87b6f000eeedb1dbff4c5248f8dc3
Paul Henschel <drcmda@gmail.com>
2018-03-23 16:44:41 +0100

    first draft

---
+++ README.md
@@ -0,0 +1,15 @@
+    npm install react-with-gesture
+
+```jsx
+import withGesture from 'react-with-gesture'
+
+@withGesture
+class Something extends React.Component {
+    render() {
+        const { x, y, down }
+        `x: ${x} y: ${y} down: ${down}`
+    }
+}
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default Wrapped =>
+    class WithGesture extends React.Component {
+        state = { x: 0, y: 0, down: false }
+
+        componentDidMount() {
+            window.addEventListener('touchmove', this.handleTouchMove)
+            window.addEventListener('touchend', this.handleMouseUp)
+            window.addEventListener('mousemove', this.handleMouseMove)
+            window.addEventListener('mouseup', this.handleMouseUp)
+        }
+
+        componentWillUnmount() {
+            window.removeEventListener('touchmove', this.handleTouchMove)
+            window.removeEventListener('touchend', this.handleMouseUp)
+            window.removeEventListener('mousemove', this.handleMouseMove)
+            window.removeEventListener('mouseup', this.handleMouseUp)
+        }
+
+        handleTouchStart = (pressLocation, e) => this.handleMouseDown(pressLocation, e.touches[0])
+        handleTouchMove = e => e.preventDefault() || this.handleMouseMove(e.touches[0])
+        handleMouseUp = () => this.setState({ down: false })
+        handleMouseDown = ({ pageX, pageY }) => this.setState({ x: 0, y: 0, initialX: pageX, initialY: pageY, down: true })
+        handleMouseMove = ({ pageX, pageY }) =>
+            this.state.down && this.setState({ x: pageX - this.state.initialX, y: pageY - this.state.initialY })
+
+        render() {
+            return (
+                <div onMouseDown={this.handleMouseDown} onTouchStart={this.handleTouchStart}>
+                    <Wrapped {...this.props} x={this.state.x} y={this.state.y} down={this.state.down} />
+                </div>
+            )
+        }
+    }
