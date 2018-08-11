0341898a4404ffe0f712fd151855db2ec7192e05
Koen Klaren <koen.klaren@gmail.com>
2018-08-11 19:13:45 +0200

    Add tests for Gesture component

---
+++ test/typescript/Gesture.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Gesture, GestureState } from '../../index';
+
+const requiredProps = (
+    <Gesture>{() => null}</Gesture>
+);
+
+const handleEvent = (gestureState: GestureState) => {
+    const {
+        x,
+        y,
+        xDelta,
+        yDelta,
+        xInitial,
+        yInitial,
+        xPrev,
+        yPrev,
+        down,
+        xVelocity,
+        yVelocity,
+    } = gestureState;
+    return gestureState;
+};
+
+const allProps = (
+    <Gesture
+        onDown={handleEvent}
+        onMove={handleEvent}
+        onUp={handleEvent}
+    >
+        {({
+            x,
+            y,
+            xDelta,
+            yDelta,
+            xInitial,
+            yInitial,
+            xPrev,
+            yPrev,
+            down,
+            xVelocity,
+            yVelocity,
+        }) => (
+            <div/>
+        )}
+    </Gesture>
+);
+
+const incorrectHandleEvent = (gestureState: GestureState) => {
+};
+const incorrectEventProp = (
+    <Gesture
+        // typings:expect-error
+        onUp={incorrectHandleEvent}
+    >
+        {() => null}
+    </Gesture>
+);
