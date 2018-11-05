e1a01a8a52df4b1d0177301978cf620a689415ac
Paul Henschel <drcmda@gmail.com>
2018-11-05 16:30:00 +0100

    refactor, kill code duplication, allow transient mode

--- src/index.js
+++ src/index.js
@@ -1,110 +1,124 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const withGesture = Wrapped =>
-  class extends React.Component {
-    static defaultProps = {
-      touch: true,
-      mouse: true,
-    }
+const initialState = {
+  x: 0,
+  y: 0,
+  xDelta: 0,
+  yDelta: 0,
+  xInitial: 0,
+  yInitial: 0,
+  xPrev: 0,
+  yPrev: 0,
+  xVelocity: 0,
+  yVelocity: 0,
+  down: false,
+}
 
-    state = {
-      x: 0,
-      y: 0,
-      xDelta: 0,
-      yDelta: 0,
-      xInitial: 0,
-      yInitial: 0,
-      xPrev: 0,
-      yPrev: 0,
-      down: false,
-    }
-
-    // Touch handlers
-    handleTouchStart = e => {
-      if (!this.props.touch) return
-      window.addEventListener('touchmove', this.handleTouchMove)
-      window.addEventListener('touchend', this.handleTouchEnd)
-      this.handleDown(e.touches[0])
-    }
-    handleTouchMove = e => {
-      this.handleMove(e.touches[0])
-    }
-    handleTouchEnd = () => {
-      window.removeEventListener('touchmove', this.handleTouchMove)
-      window.removeEventListener('touchend', this.handleMouseUp)
-      this.handleUp()
-    }
-
-    // Mouse handlers
-    handleMouseDown = e => {
-      if (!this.props.mouse) return
-      window.addEventListener('mousemove', this.handleMouseMoveRaf)
-      window.addEventListener('mouseup', this.handleMouseUp)
-      this.handleDown(e)
-    }
-    handleMouseMove = ({ pageX, pageY }) => {
-      if (!this._busy) {
-        requestAnimationFrame(() => {
-          this.handleMove({
-            pageX,
-            pageY,
-          })
-        })
-        this._busy = true
-      }
-    }
-    handleMouseUp = () => {
-      window.removeEventListener('mousemove', this.handleMouseMove)
-      window.removeEventListener('mouseup', this.handleMouseUp)
-      this.handleUp()
-    }
-
-    // Common handlers
-    handleDown = ({ pageX, pageY }) => {
+function handlers(set, props) {
+  // Common handlers
+  const handleUp = () =>
+    set(state => {
+      const newProps = { ...state, down: false }
+      props.onUp && props.onUp(newProps)
+      return newProps
+    })
+  const handleDown = ({ pageX, pageY }) =>
+    set(state => {
       const newProps = {
-        ...this.state,
+        ...state,
         x: pageX,
         y: pageY,
         xDelta: 0,
         yDelta: 0,
+        xVelocity: 0,
+        yVelocity: 0,
         xInitial: pageX,
         yInitial: pageY,
         xPrev: pageX,
         yPrev: pageY,
         down: true,
       }
-      this.setState(this.props.onDown ? this.props.onDown(newProps) : newProps)
-    }
-    handleMove = ({ pageX, pageY }) => {
+      props.onDown && props.onDown(newProps)
+      return newProps
+    })
+  const handleMove = ({ pageX, pageY, movementX, movementY }) =>
+    set(state => {
       const newProps = {
-        ...this.state,
+        ...state,
         x: pageX,
         y: pageY,
-        xDelta: pageX - this.state.xInitial,
-        yDelta: pageY - this.state.yInitial,
-        xPrev: this.state.x,
-        yPrev: this.state.y,
-        xVelocity: pageX - this.state.x,
-        yVelocity: pageY - this.state.y,
+        xDelta: pageX - state.xInitial,
+        yDelta: pageY - state.yInitial,
+        xPrev: state.x,
+        yPrev: state.y,
+        xVelocity: movementX,
+        yVelocity: movementY,
       }
-      this.setState(this.props.onMove ? this.props.onMove(newProps) : newProps, () => (this._busy = false))
+      props.onMove && props.onMove(newProps)
+      return newProps
+    })
+
+  // Touch handlers
+  const handleTouchStart = e => {
+    window.addEventListener('touchmove', handleTouchMove)
+    window.addEventListener('touchend', handleTouchEnd)
+    handleDown(e.touches[0])
+  }
+  const handleTouchMove = e => handleMove(e.touches[0])
+  const handleTouchEnd = () => {
+    window.removeEventListener('touchmove', handleTouchMove)
+    window.removeEventListener('touchend', handleMouseUp)
+    handleUp()
+  }
+
+  // Mouse handlers
+  const handleMouseDown = e => {
+    window.addEventListener('mousemove', handleMove)
+    window.addEventListener('mouseup', handleMouseUp)
+    handleDown(e)
+  }
+  const handleMouseUp = () => {
+    window.removeEventListener('mousemove', handleMove)
+    window.removeEventListener('mouseup', handleMouseUp)
+    handleUp()
+  }
+  return {
+    onMouseDown: handleMouseDown,
+    onTouchStart: handleTouchStart,
+  }
+}
+
+const withGesture = Wrapped =>
+  class extends React.Component {
+    static propTypes = {
+      /** When this holds true it will manage its state outside of React, in this case it will never ever
+       cause a new render, clients have to rely on callbacks to get notified (onUp/Down/Move). */
+      transient: PropTypes.bool,
+      /** Optional. Calls back on mouse-down or surface touched */
+      onDown: PropTypes.func,
+      /** Optional. Calls back on mouse-up or touch lifted */
+      onUp: PropTypes.func,
+      /** Optional. Calls back on mouse/touch move */
+      onMove: PropTypes.func,
     }
-    handleUp = () => {
-      const newProps = {
-        ...this.state,
-        down: false,
+    static defaultProps = { touch: true, mouse: true }
+
+    constructor(props) {
+      super(props)
+      this.state = initialstate
+      const set = this.setState.bind(this)
+      if (props.transient) {
+        this._state = initialstate
+        set = cb => (this._state = cb(this._state))
       }
-      this.setState(this.props.onUp ? this.props.onUp(newProps) : newProps)
+      this.handlers = handlers(set, props)
     }
 
     render() {
       const { style, className, ...props } = this.props
       return (
-        <div
-          onMouseDown={this.handleMouseDown}
-          onTouchStart={this.handleTouchStart}
-          style={{ display: 'contents', ...style }}
-          className={className}>
+        <div {...this.handlers} style={{ display: 'contents', ...style }} className={className}>
           <Wrapped {...props} {...this.state} />
         </div>
       )
@@ -119,82 +133,13 @@ const Gesture = withGesture(
   },
 )
 
-// TODO: take care of code duplication before releasing 2.0
-function useGesture() {
-  const [state, set] = React.useState({
-    x: 0,
-    y: 0,
-    xDelta: 0,
-    yDelta: 0,
-    xInitial: 0,
-    yInitial: 0,
-    xPrev: 0,
-    yPrev: 0,
-    down: false
-  })
-
-  const [handlers] = React.useState(() => {
-    // Common handlers
-    const handleDown = ({ pageX, pageY }) => {
-      set(state => ({
-        ...state,
-        x: pageX,
-        y: pageY,
-        xDelta: 0,
-        yDelta: 0,
-        xInitial: pageX,
-        yInitial: pageY,
-        xPrev: pageX,
-        yPrev: pageY,
-        down: true
-      }))
-    }
-    const handleMove = ({ pageX, pageY }) => {
-      set(state => ({
-        ...state,
-        x: pageX,
-        y: pageY,
-        xDelta: pageX - state.xInitial,
-        yDelta: pageY - state.yInitial,
-        xPrev: state.x,
-        yPrev: state.y,
-        xVelocity: pageX - state.x,
-        yVelocity: pageY - state.y
-      }))
-    }
-    const handleUp = () => set(state => ({ ...state, down: false }))
-
-    // Touch handlers
-    const handleTouchStart = e => {
-      window.addEventListener('touchmove', handleTouchMove)
-      window.addEventListener('touchend', handleTouchEnd)
-      handleDown(e.touches[0])
-    }
-    const handleTouchMove = e => handleMove(e.touches[0])
-    const handleTouchEnd = () => {
-      window.removeEventListener('touchmove', handleTouchMove)
-      window.removeEventListener('touchend', handleMouseUp)
-      handleUp()
-    }
-
-    // Mouse handlers
-    const handleMouseDown = e => {
-      window.addEventListener('mousemove', handleMouseMove)
-      window.addEventListener('mouseup', handleMouseUp)
-      handleDown(e)
-    }
-    const handleMouseMove = ({ pageX, pageY }) => handleMove({ pageX, pageY })
-    const handleMouseUp = () => {
-      window.removeEventListener('mousemove', handleMouseMove)
-      window.removeEventListener('mouseup', handleMouseUp)
-      handleUp()
-    }
-    return {
-      onMouseDown: handleMouseDown,
-      onTouchStart: handleTouchStart
-    }
-  })
-  return [handlers, state]
+function useGesture(props) {
+  const [state, set] = React.useState(initialState)
+  const transientState = React.useRef(initialState)
+  const [spread] = React.useState(() =>
+    handlers(props && props.transient ? cb => (transientState.current = cb(transientState.current)) : set, props),
+  )
+  return [spread, state]
 }
 
 export { withGesture, Gesture, useGesture }
