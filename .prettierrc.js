2fcc6b2fe32fb76b0ef52b000c004058986c06ee
Tim Ellenberger <timellenberger@gmail.com>
2019-03-08 12:29:01 -0800

    Add basic prettier config and pre-commit hook

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * Prettier auto code formatting options
+ *
+ * https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2
+}
