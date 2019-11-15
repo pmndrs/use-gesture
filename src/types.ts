986808b7553dc079b68d9cc28d6dd9081958c3d7
David Bismut <david.bismut@gmail.com>
2019-11-15 13:02:08 +0100

    type: removed label type used for testing

--- src/types.ts
+++ src/types.ts
@@ -17,7 +17,6 @@ export interface GenericConfig {
   window?: EventTarget
   eventOptions: Partial<EventOptions & { pointer: boolean }>
   enabled: boolean
-  label?: string
 }
 
 export interface DragConfig {
@@ -53,7 +52,7 @@ export interface InternalDragConfig {
   axis?: 'x' | 'y'
 }
 
-export type InternalFullConfig = InternalGenericConfig & { drag?: InternalDragConfig; label?: string }
+export type InternalFullConfig = InternalGenericConfig & { drag?: InternalDragConfig }
 
 export type WebKitGestureEvent = React.PointerEvent & { scale: number; rotation: number }
 export type UseGestureEvent<
