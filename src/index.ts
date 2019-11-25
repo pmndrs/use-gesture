2a65c168480df622d68e15faf41d9dcd331997e8
David Bismut <david.bismut@gmail.com>
2019-11-25 10:59:01 +0100

    fix: added useWheel to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { addV, subV } from './utils/math'
 export { useDrag } from './hooks/useDrag'
-// export { useWheel } from './hooks/useWheel'
+export { useWheel } from './hooks/useWheel'
 export { useGesture } from './hooks/useGesture'
