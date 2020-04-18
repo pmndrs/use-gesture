5c8c486dec1aff5d241861b487fc3d1397b12be2
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2020-04-18 05:55:10 +0300

    remove rubberband from utils

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -78,33 +78,3 @@ export function calculateAllKinematics<T extends number[]>(movement: T, delta: T
 }
 
 
-function minMax(value: number, min: number, max: number) {
-  return Math.max(min, Math.min(value, max))
-}
-
-// Based on @aholachek ;)
-// https://twitter.com/chpwn/status/285540192096497664
-// iOS constant = 0.55
-
-// https://medium.com/@nathangitter/building-fluid-interfaces-ios-swift-9732bb934bf5
-function rubberband2(distance: number, constant: number) {
-  // default constant from the article is 0.7
-  return Math.pow(distance, constant * 5)
-}
-
-function rubberband(distance: number, dimension: number, constant: number) {
-  if (dimension === 0 || Math.abs(dimension) === Infinity) return rubberband2(distance, constant)
-  return (distance * dimension * constant) / (dimension + constant * distance)
-}
-
-export function rubberbandIfOutOfBounds(position: number, min: number, max: number, constant = 0.15) {
-  if (constant === 0) return minMax(position, min, max)
-
-  if (position < min) {
-    return -rubberband(min - position, max - min, constant) + min
-  }
-  if (position > max) {
-    return rubberband(position - max, max - min, constant) + max
-  }
-  return position
-}
