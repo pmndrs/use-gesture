d3cacd6ad71c6a49c1c5b4271060ddcb4d69bbd9
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2020-04-20 04:12:12 +0300

    rework chainFns

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,14 @@
-import { Fn, Vector2 } from '../types'
+import { Vector2 } from '../types'
 
 // blank function
 export function noop() {}
-// returns a function that chains all functions given as parameters
-export const chainFns = (...fns: Fn[]): Fn => (...args: any[]) => fns.forEach(fn => fn(...args))
+
+export function chainFns(...fns: Function[]) {
+  return function (this: any) {
+    for (let fn of fns) fn.apply(this, arguments)
+  }
+}
+
 
 export const def = {
   array: <T>(value: T | T[]): T[] => (Array.isArray(value) ? value : [value, value]),
