900df2fc893b29c26d1b01e429bced585192a285
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2020-04-20 04:36:47 +0300

    Temporary typing fix

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,7 @@ export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 export type AtLeastOneOf<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
 
 export type Vector2 = [ number, number ]
-export type Fn = (...args: any[]) => any
+export type Fn = any
 
 export interface AxisBounds {
   top?: number
