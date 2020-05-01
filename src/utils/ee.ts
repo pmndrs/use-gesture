b282847eeeffe222db2388a472368e4aa08af279
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2020-05-01 22:52:16 +0300

    use default export

--- src/utils/ee.ts
+++ src/utils/ee.ts
@@ -1,13 +1,17 @@
 type Handler<T> = (data: T) => void
 
-export const TinyEmitter = <T>(S = new Set<Handler<T>>()) => ({
-  listen: (fn: Handler<T>) => {
-    S.add(fn)
-  },
-  unlisten: (fn: Handler<T>) => {
-    S.delete(fn)
-  },
-  emit: (data: T) => {
-    for (let h of S) h(data)
-  },
-})
+export default function EE <T>() {
+  let S = new Set<Handler<T>>()
+
+  return {
+    listen: (fn: Handler<T>) => {
+      S.add(fn)
+    },
+    unlisten: (fn: Handler<T>) => {
+      S.delete(fn)
+    },
+    emit: (data: T) => {
+      for (let h of S) h(data)
+    },
+  }
+}
