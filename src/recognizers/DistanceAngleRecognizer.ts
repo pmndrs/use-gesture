a32cc9fdc34218a74be381fa694d01b431bd1686
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2020-05-09 22:54:02 +0300

    Clean up

--- src/recognizers/DistanceAngleRecognizer.ts
+++ src/recognizers/DistanceAngleRecognizer.ts
@@ -5,10 +5,6 @@ import { Vector2, UseGestureEvent, PartialGestureState, DistanceAngleKey, Gestur
 /**
  * @private
  * Abstract class for distance/angle-based gesture recongizers
- * @abstract
- * @class DistanceAngleRecognizer
- * @extends {Recognizer<T>}
- * @template T
  */
 export default abstract class DistanceAngleRecognizer<T extends DistanceAngleKey> extends Recognizer<T> {
   /**
@@ -18,39 +14,21 @@ export default abstract class DistanceAngleRecognizer<T extends DistanceAngleKey
     // angle might not be defined when ctrl wheel is used for zoom only
     // in that case we set it to the previous angle value
     a = a !== void 0 ? a : state.values[1]
-
     let delta_a = a - state.values[1]
-
-    //console.warn({
-    //  raw: delta_a, 
-    //  fixed: fixContinuity(delta_a)
-    //})
-
     let next_turns = state.turns
-    if (Math.abs(delta_a) > 270) {
-      next_turns += Math.sign(delta_a)
-    }
-
-    /**
-     * The angle value might jump from 179deg to -179deg when we actually want to
-     * read 181deg to ensure continuity. To make that happen, we detect when the jump
-     * is supsiciously high (ie > 270deg) and increase the `turns` value
-     */
-
+    if (Math.abs(delta_a) > 270) next_turns += Math.sign(delta_a)
     return subV([ d, a - 360 * next_turns ], state.initial)
   }
 
   getKinematics(values: Vector2, event: UseGestureEvent): PartialGestureState<T> {
-    const movementDetection = this.getMovement(values)
-    const turns = (values[1] - movementDetection.movement![1] - this.state.initial[1]) / 360
+    const state = this.getMovement(values)
+    const turns = (values[1] - state.movement![1] - this.state.initial[1]) / 360
 
-    const kinematics = calculateAllKinematics(
-      movementDetection.movement!, 
-      movementDetection.delta!, 
-      event.timeStamp - this.state.timeStamp!
-    )
+    const dt = event.timeStamp - this.state.timeStamp!
 
-    return { values, turns, ...movementDetection, ...kinematics }
+    const kinematics = calculateAllKinematics(state.movement!, state.delta!, dt)
+
+    return { turns, ...state, ...kinematics }
   }
 
   protected mapStateValues(state: GestureState<T>): PartialGestureState<T> {
