d2753f9339688396260a936304bb299c5632c571
Kwoncheol Shin <hello.kwon.charles@gmail.com>
2020-08-30 11:21:49 +0900

    feat: replaced the Math.sign polyfill function with a function suggested by Mozilla

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -77,12 +77,15 @@ export function calculateAllKinematics<T extends number[]>(movement: T, delta: T
   }
 }
 
+/**
+ * Because IE doesn't support `Math.sign` function, so we use the polyfill version of the function.
+ * This polyfill function came from
+ * :https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign#Polyfill
+ * @param x target number
+ */
 export function sign(x: number) {
-  let num = Number(x)
-  if (num === 0 || isNaN(num)) {
-    return num
-  }
-  return num > 0 ? 1 : -1
+  if (Math.sign) return Math.sign(x)
+  return Number(x > 0) - Number(x < 0) || +x
 }
 
 export function getIntentionalDisplacement(movement: number, threshold: number): number | false {
