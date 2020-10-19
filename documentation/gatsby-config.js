bd71069b931bdda41b83530822e1c26f88f3fd01
David <david.bismut@gmail.com>
2020-10-19 18:25:54 +0200

    docs update

--- documentation/gatsby-config.js
+++ documentation/gatsby-config.js
@@ -5,12 +5,18 @@ module.exports = {
       options: {
         name: 'React UseGesture',
         siteUrl: 'https://use-gesture.netlify.app',
-        description: `The only gesture hook you'll need`,
+        description: `React UseGesture allows you to implement advanced UI interactions with just a few lines of code.`,
         author: 'David Bismut',
         sections: ['About', 'Reference', 'More'],
         navItems: [{ title: 'Docs', url: '/docs/' }],
         twitterAccount: 'pmndrs',
         githubRepositoryURL: 'https://github.com/pmndrs/react-use-gesture',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-162749258-1',
       },
     },
   ],
