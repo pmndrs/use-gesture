c423c7f4645ac2ee2fc33fc20928e7b05baef03b
David <david.bismut@gmail.com>
2020-10-20 17:37:29 +0200

    example: fix

--- examples/src/pinch/index.tsx
+++ examples/src/pinch/index.tsx
@@ -3,49 +3,28 @@ import { useSpring, animated } from 'react-spring'
 import { useGesture } from 'react-use-gesture'
 import styles from './styles.css'
 
-document.addEventListener('gesturestart', e => e.preventDefault())
-document.addEventListener('gesturechange', e => e.preventDefault())
+// document.addEventListener('gesturestart', e => e.preventDefault())
+// document.addEventListener('gesturechange', e => e.preventDefault())
 
 export default function Pinch() {
-  const [style1, set1] = useSpring(() => ({ x: 0, y: 0, rotateZ: 0, scale: 1 }))
-  const [style2, set2] = useSpring(() => ({ x: 0, y: 0, rotateZ: 0, scale: 1 }))
-  const domTarget1 = React.useRef(null)
-  const domTarget2 = React.useRef(null)
+  const [style, set] = useSpring(() => ({ x: 0, y: 0, rotateZ: 0, scale: 1 }))
+  const domTarget = React.useRef(null)
 
-  const bind1 = useGesture(
+  useGesture(
     {
-      // onHover: ({ hovering }) => {
-      //   set({ scale: hovering ? 1.3 : 1 })
-      // },
       onDrag: ({ offset: [x, y] }) => {
-        set1({ x, y })
+        set({ x, y })
       },
       onPinch: ({ offset: [d, a] }) => {
-        set1({ scale: 1 + d / 200, rotateZ: a })
+        set({ scale: 1 + d / 200, rotateZ: a })
       },
-    }
-    // { domTarget: domTarget1 }
-  )
-
-  const bind2 = useGesture(
-    {
-      // onHover: ({ hovering }) => {
-      //   set({ scale: hovering ? 1.3 : 1 })
-      // },
-      onDrag: ({ offset: [x, y] }) => {
-        set2({ x, y })
-      },
-      onPinch: ({ offset: [d, a] }) => {
-        set2({ scale: 1 + d / 200, rotateZ: a })
-      },
-    }
-    // { domTarget: domTarget2 }
+    },
+    { domTarget: domTarget }
   )
 
   return (
     <div className={`${styles.simple} flex`}>
-      <animated.div id="one" ref={domTarget1} {...bind1()} style={style1} />
-      <animated.div id="two" ref={domTarget2} {...bind2()} style={style2} />
+      <animated.div id="one" ref={domTarget} style={style} />
     </div>
   )
 }
