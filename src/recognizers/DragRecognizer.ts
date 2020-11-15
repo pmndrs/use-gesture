2bb16acd0b48e7ca4268ef615cf498fc363b316f
David <david.bismut@gmail.com>
2020-11-15 18:26:02 +0100

    fix: release pointer only if capture is still set

--- src/recognizers/DragRecognizer.ts
+++ src/recognizers/DragRecognizer.ts
@@ -34,7 +34,7 @@ export class DragRecognizer extends CoordinatesRecognizer<'drag'> {
     if (_dragPointerId && _dragTarget && 'releasePointerCapture' in _dragTarget) {
       // this would work in the DOM but doesn't with react three fiber
       // target.removeEventListener('pointermove', this.onDragChange, this.controller.config.eventOptions)
-      _dragTarget.releasePointerCapture(_dragPointerId)
+      _dragTarget.hasPointerCapture(_dragPointerId) && _dragTarget.releasePointerCapture(_dragPointerId)
     }
   }
 
