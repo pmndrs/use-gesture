a09716addc9c076b79cd397d9e6f94689e6cd4e0
David <david.bismut@gmail.com>
2020-11-21 15:40:05 +0100

    docs: update

--- documentation/pages/docs/code/examples.js
+++ documentation/pages/docs/code/examples.js
@@ -1,7 +1,7 @@
 import React, { Suspense, useState, useEffect, useRef } from 'react'
 import { useSpring, useSprings, animated, to } from 'react-spring'
 import { a as a3f } from '@react-spring/three'
-import { Canvas, useThree } from 'react-three-fiber'
+import { Canvas, useThree, useFrame } from 'react-three-fiber'
 import { useTexture } from '@react-three/drei'
 import { useDrag, useScroll, useGesture, useWheel } from 'react-use-gesture'
 import { Lethargy } from 'lethargy'
@@ -514,6 +514,39 @@ export function PreventWindowScrollY() {
         <a3f.mesh {...bind()} rotation={rot} geometry={torusknot} material={material} scale={scale} />
         <Environment />
       </Suspense>
+    </Canvas>
+  )
+}
+
+function Box() {
+  const mesh = useRef()
+
+  const { viewport } = useThree()
+  const { width, height, factor } = viewport
+  const [spring, setSpring] = useSpring(() => ({ position: [0, 0, 0], scale: [1, 1, 1] }))
+
+  const bind = useDrag(({ offset: [x, y] }) => setSpring({ position: [x, y, 0] }), {
+    bounds: { left: -width / 2.2, right: width / 2.2, top: -height / 2.2, bottom: height / 2.2 },
+    rubberband: true,
+    transform: ([x, y]) => [x / factor, -y / factor],
+  })
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  return (
+    <a3f.mesh ref={mesh} {...bind()} {...spring}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </a3f.mesh>
+  )
+}
+
+export function Transform() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <Box />
     </Canvas>
   )
 }
