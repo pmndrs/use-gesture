7f05056a3214005f7300ec4f4dce830dfc0eb773
David <david.bismut@gmail.com>
2020-12-02 13:18:29 +0100

    examples: update for down

--- examples/src/viewpager/index.tsx
+++ examples/src/viewpager/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.css'
 const pages = [
   'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/1467435/pexels-photo-1467435.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
   'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 ]
@@ -19,16 +19,16 @@ export default function Viewpager() {
     scale: 1,
     display: 'block',
   }))
-  const bind = useDrag(({ down, movement: [mx], direction: [xDir], distance, cancel }) => {
-    if (down && distance > window.innerWidth / 2) {
+  const bind = useDrag(({ active, movement: [mx], direction: [xDir], distance, cancel }) => {
+    if (active && distance > window.innerWidth / 2) {
       index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, pages.length - 1)
       cancel()
     }
 
     set(i => {
       if (i < index.current - 1 || i > index.current + 1) return { display: 'none' }
-      const x = (i - index.current) * window.innerWidth + (down ? mx : 0)
-      const scale = down ? 1 - distance / window.innerWidth / 2 : 1
+      const x = (i - index.current) * window.innerWidth + (active ? mx : 0)
+      const scale = active ? 1 - distance / window.innerWidth / 2 : 1
       return { x, scale, display: 'block' }
     })
   })
