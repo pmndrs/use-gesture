cc25f672bdf96256aef266985697756b241981e9
David Bismut <david.bismut@gmail.com>
2021-04-27 17:42:54 +0200

    feat: vanilla gesture

---
+++ packages/vanilla/src/Gesture.js
@@ -0,0 +1,19 @@
+import {
+  registerEngine,
+  DragEngine,
+  PinchEngine,
+  ScrollEngine,
+  WheelEngine,
+  parseMergedHandlers
+} from '@use-gesture/core'
+import { Recognizer } from './Recognizer'
+
+registerEngine('drag', DragEngine)
+registerEngine('pinch', PinchEngine)
+registerEngine('scroll', ScrollEngine)
+registerEngine('wheel', WheelEngine)
+
+export function Gesture(target, _handlers, _config = {}) {
+  const { handlers, nativeHandlers, config } = parseMergedHandlers(_handlers, _config)
+  return new Recognizer(target, handlers, config, undefined, nativeHandlers)
+}
