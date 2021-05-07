7bad6e00dd43516e624eb8cf002d822304f3f640
David Bismut <david.bismut@gmail.com>
2021-05-07 12:52:07 +0200

    fix: fix axis detection

--- packages/core/src/engines/CoordinatesEngine.ts
+++ packages/core/src/engines/CoordinatesEngine.ts
@@ -55,9 +55,8 @@ CoordinatesEngine.prototype.intent = function (v) {
   if (this.config.lockDirection) {
     if (state.axis === 'x') v[1] = 0
     else if (state.axis === 'y') v[0] = 0
-  } else if (state.axis && this.config.axis) {
+  } else if (this.config.axis) {
     if (state.axis !== this.config.axis) {
-      state._active = false
       state._blocked = true
     } else if (state.axis === 'x') v[1] = 0
     else if (state.axis === 'y') v[0] = 0
