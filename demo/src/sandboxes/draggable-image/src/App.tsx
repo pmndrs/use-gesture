29eda7776693c8b945d49e6683d6ae4e82225151
David Bismut <david.bismut@gmail.com>
2021-09-08 11:00:32 +0200

    demo: add link draggable example

--- demo/src/sandboxes/draggable-image/src/App.tsx
+++ demo/src/sandboxes/draggable-image/src/App.tsx
@@ -3,7 +3,7 @@ import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 import styles from './styles.module.css'
 
-export default function App() {
+function Image() {
   const [props, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }))
   const bind = useDrag(({ event, active, movement: [x, y] }) => {
     event.preventDefault()
@@ -15,13 +15,50 @@ export default function App() {
     })
   })
   return (
+    <animated.img
+      className={`${styles.drag} ${styles.img}`}
+      src="https://images.unsplash.com/photo-1611149043085-c4d0a79304c6?&auto=format&fit=crop&w=400&q=80"
+      {...bind()}
+      style={props}
+    />
+  )
+}
+
+function Link() {
+  const [props, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }))
+  const bind = useDrag(
+    ({ event, intentional, active, offset: [x, y] }) => {
+      event.preventDefault()
+      if (intentional) {
+        api.start({
+          x,
+          y,
+          scale: active ? 1.2 : 1,
+          immediate: (k) => k !== 'scale' && active
+        })
+      }
+    },
+    { filterTaps: true, triggerAllEvents: true }
+  )
+  return (
+    <animated.a
+      className={styles.drag}
+      {...bind()}
+      style={props}
+      href="https://github.com/pmndrs/use-gesture"
+      target="_blank"
+      rel="noopener,noreferrer"
+    >
+      This is a link
+    </animated.a>
+  )
+}
+
+export default function App() {
+  return (
     <div className="flex fill center">
-      <animated.img
-        className={styles.drag}
-        src="https://images.unsplash.com/photo-1611149043085-c4d0a79304c6?&auto=format&fit=crop&w=400&q=80"
-        {...bind()}
-        style={props}
-      />
+      <Link />
+      <Image />
     </div>
   )
 }
