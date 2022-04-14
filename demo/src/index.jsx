210eb0716707a8beb91aa2f363f6b27d20d3aa3f
David Bismut <david.bismut@gmail.com>
2022-04-14 19:25:21 +0200

    chore: upgrading to React 18 (#484)

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
 import './index.css'
@@ -7,9 +7,9 @@ import './index.css'
 // window.screenLog.init()
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(
+const root = createRoot(rootElement)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 )
