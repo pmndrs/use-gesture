59789ebb7ade768a48790e1890dbc6cd7171132a
David Bismut <david.bismut@gmail.com>
2022-04-14 19:16:41 +0200

    chore: upgrading to React 18

--- demo/src/sandboxes/gesture-simplest/src/App.tsx
+++ demo/src/sandboxes/gesture-simplest/src/App.tsx
@@ -6,13 +6,16 @@ import styles from './styles.module.css'
 
 export default function App() {
   const [style, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }))
-  const bind = useDrag(({ active, movement: [x, y] }) => {
-    api.start({
-      x: active ? x : 0,
-      y: active ? y : 0,
-      scale: active ? 1.2 : 1
-    })
-  })
+  const bind = useDrag(
+    ({ active, offset: [x, y] }) => {
+      api.start({
+        x: active ? x : 0,
+        y: active ? y : 0,
+        scale: active ? 1.2 : 1
+      })
+    },
+    { bounds: { left: -30 } }
+  )
 
   return (
     <div className="flex fill center">
--- demo/src/sandboxes/infinite-slideshow/src/index.jsx
+++ demo/src/sandboxes/infinite-slideshow/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(
+const root = createRoot(rootElement)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 )
