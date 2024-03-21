538e282612aaf409f6ecb99eeaab6fcacdfbe18f
David Bismut <david.bismut@gmail.com>
2024-03-21 16:00:32 +0100

    docs: fix random() SSR bug

--- documentation/pages/docs/code/rug.js
+++ documentation/pages/docs/code/rug.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { EasterDiv } from './examples'
 
 const handMojis = ['👆', '👇', '👈', '🤘', '🤙', '✊', '🖖', '🖐', '🤞', '👍', '👊']
 const randomEmoji = () => handMojis[Math.floor(Math.random() * handMojis.length)]
 
 export default function Rug({ pkg }) {
+  const [emoji, setEmoji] = React.useState(handMojis[0])
+
+  useEffect(() => setEmoji(randomEmoji()), [])
+
   return (
     <EasterDiv>
       <span style={{ color: '#ff9800', touchAction: 'none' }}>
-        {randomEmoji()} <strong>@use-gesture</strong>
+        {emoji} <strong>@use-gesture</strong>
         {pkg && `/${pkg}`}
       </span>
     </EasterDiv>
